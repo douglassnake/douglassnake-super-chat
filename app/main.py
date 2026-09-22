1237fb99e15ca5a450ea9f9d6ce9baa558570398
douglassouza <97610500+douglassnake@users.noreply.github.com>
2026-09-22 15:47:11 -0300

    chore: marcar API 0.8.12

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ from app.session_routes import router as session_router
 from app.worker_attempt_routes import router as worker_attempt_router
 
 settings = get_settings()
-app = FastAPI(title=settings.app_name, version="0.8.11")
+app = FastAPI(title=settings.app_name, version="0.8.12")
 
 
 @app.get("/health")
